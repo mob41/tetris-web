30d2730fae2ea70b058e94a073078aa2920d65e9
mob41 <puihuenlaw@gmail.com>
2019-04-12 20:52:27 +0800

    add assets

--- js/tetris-web.js
+++ js/tetris-web.js
@@ -1,21 +1,74 @@
 var camera, scene, renderer;
 var geometry, material, mesh, text;
 
+var rectMaterial;
+var mainRect, mainRectGeo;
+
+var mainPlane, mainPlaneGeo, mainPlaneMaterial;
+
+var bgm;
+
 init();
 animate();
 
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
+}
+
+var bgmInit = false;
+$("body,html").bind("touchstart touchmove scroll mousedown DOMMouseScroll mousewheel keyup", function (e) {
+    if (!bgmInit){
+        bgmInit = true;
+        bgm.play();
+    }
+});  
+
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 0.3;
+    bgm = new sound("assets/bgm.mp3");
+    bgm.sound.addEventListener('ended', function () {
+        console.log("repeat");
+        bgm.play();
+    }, false);
 
-	scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x737373);
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+    geometry = new THREE.BoxGeometry(5, 5, 5);
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    rectMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+
+    mainRectGeo = new THREE.Geometry();
+    mainRectGeo.vertices.push(new THREE.Vector3(-10, 0, 0));
+    mainRectGeo.vertices.push(new THREE.Vector3(0, 10, 0));
+    mainRectGeo.vertices.push(new THREE.Vector3(10, 0, 0));
+
+    mainRect = new THREE.Line(mainRectGeo, rectMaterial);
+    scene.add(mainRect);
+
+    mainPlaneMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+    mainPlaneGeo = new THREE.PlaneGeometry(40, 60);
+    mainPlane = new THREE.Mesh(mainPlaneGeo, mainPlaneMaterial);
+    scene.add(mainPlane);
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
